81e1950d4daf628c60c96401237773ba40fb9e68
Nur Hambali <chorongbali@gmail.com>
2024-12-05 15:43:52 +0800

    updates

--- elkawstfpersonal/tf/dev/scripts/metricbeat_start.sh
+++ elkawstfpersonal/tf/dev/scripts/metricbeat_start.sh
@@ -12,9 +12,14 @@ sudo apt-get install apt-transport-https -y
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 sudo apt-get update && sudo apt-get install metricbeat -y
 
+# backup current configuration
+sudo cp /etc/metricbeat/metricbeat.yml /etc/metricbeat/metricbeat.yml.bak
+
 # Configure Metricbeat to send metrics to Elasticsearch
-sudo sed -i "s|#output.elasticsearch:|output.elasticsearch:|" /etc/metricbeat/metricbeat.yml
-sudo sed -i "s|#  hosts: \[\"localhost:9200\"\]|  hosts: [\"${elasticsearch_ip}:9200\"]|" /etc/metricbeat/metricbeat.yml
+# The line is now uncommented, enabling the output.elasticsearch configuration.
+sudo sed -i "s|#output.elasticsearch:.*|output.elasticsearch:|" /etc/metricbeat/metricbeat.yml
+# Now we insert our elasticsearch ip in the metricbeat configuration file
+sudo sed -i "/output.elasticsearch:/,/^[^ ]/ s|#\? *hosts:.*|  hosts: [\"${elasticsearch_ip}:9200\"]|" /etc/metricbeat/metricbeat.yml
 
 # Enable and configure the Metricbeat system module
 sudo metricbeat modules enable system
